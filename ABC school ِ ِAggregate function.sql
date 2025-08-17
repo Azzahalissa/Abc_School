d53bade7bdb5e03a3a5ccf65db58c55b6918e7db
Azza Alissa <azzahalissa@gmail.com>
2025-08-18 02:44:13 +0300

    Add files via upload

---
+++ ABC school ِ ِAggregate function.sql
@@ -0,0 +1,6 @@
+use abc_school ; -- use database 
+select sum(GPA) As Total_marks from students; -- total marks
+select avg(GPA) As Average_marks from students; -- average marks
+select max(GPA) As Highest_marks from students; -- max marks
+select min(GPA) As Lowest_marks from students; -- min marks
+select count(*) As total_students from students; -- total students countٍٍ
